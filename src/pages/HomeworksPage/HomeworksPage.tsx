369811d7d3a8236fb1d0936071d56052611026fd
Aliya <saigafarova00@yandex.ru>
2022-03-14 17:50:45 +0300

    add: drop-down filter

--- src/pages/HomeworksPage/HomeworksPage.tsx
+++ src/pages/HomeworksPage/HomeworksPage.tsx
@@ -1,5 +1,33 @@
+import {FilterList} from "../../components/FilterList";
+
 export const HomeworksPage = () => {
   return (
-    <div>Домашки</div>
+    <div>Домашки
+      <FilterList data={[{
+        id: 1, name:'Все'
+      },
+        {
+          id: 2, name:'Эта неделя'
+        },
+        {
+          id: 3, name:'Этот месяц'
+        }]} type=''/>
+        <div> </div>
+        <FilterList data={[{
+            id: 1, name:'Все'
+        },
+            {
+                id: 2, name:'Эта неделя'
+            },
+            {
+                id: 3, name:'Этот месяц'
+            },
+            {
+                id: 4, name:'Этот год'
+            },
+            {
+                id: 5, name:'Это десятилетие'
+            },]} type='table'/>
+    </div>
   )
 }
