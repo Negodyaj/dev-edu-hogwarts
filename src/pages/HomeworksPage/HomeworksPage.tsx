35de7e860978a66e7fa0c321d0ccf0ec59d644d6
Aliya <saigafarova00@yandex.ru>
2022-03-16 23:57:57 +0300

    add: date picker, "react-datetime" library

--- src/pages/HomeworksPage/HomeworksPage.tsx
+++ src/pages/HomeworksPage/HomeworksPage.tsx
@@ -1,43 +1,44 @@
 import {FilterList} from "../../components/FilterList";
 import {HomeworkCard} from "./components/HomeworkCard";
+import {Datepicker} from "../../components/Datepicker/Datepicker";
 
 let tasks = [
-    {
-        id: 4,
-        taskNumber: 4,
-        title: 'Покрыть тестами первые три домашки',
-        dateBeginning: '10.10.2022',
-        dateEnd: '05.11.2022',
-        status: 4,
-        elseData: '',
-    },
-    {
-        id: 3,
-        taskNumber: 3,
-        title: 'Покрыть тестами первые три домашки Покрыть тестами первые три домашки Покрыть тестами первые три домашки',
-        dateBeginning: '10.10.2022',
-        dateEnd: '01.11.2022',
-        status: 2,
-        elseData: '',
-    },
-    {
-        id: 2,
-        taskNumber: 2,
-        title: 'Покрыть тестами первые три домашки',
-        dateBeginning: '28.09.2022',
-        dateEnd: '14.09.2022',
-        status: 1,
-        elseData: '',
-    },
-    {
-        id: 1,
-        taskNumber: 1,
-        title: 'Покрыть тестами первые три домашки',
-        dateBeginning: '10.10.2022',
-        dateEnd: '05.11.2022',
-        status: 0,
-        elseData: '',
-    }
+  {
+    id: 4,
+    taskNumber: 4,
+    title: 'Покрыть тестами первые три домашки',
+    dateBeginning: '10.10.2022',
+    dateEnd: '05.11.2022',
+    status: 4,
+    elseData: '',
+  },
+  {
+    id: 3,
+    taskNumber: 3,
+    title: 'Покрыть тестами первые три домашки Покрыть тестами первые три домашки Покрыть тестами первые три домашки',
+    dateBeginning: '10.10.2022',
+    dateEnd: '01.11.2022',
+    status: 2,
+    elseData: '',
+  },
+  {
+    id: 2,
+    taskNumber: 2,
+    title: 'Покрыть тестами первые три домашки',
+    dateBeginning: '28.09.2022',
+    dateEnd: '14.09.2022',
+    status: 1,
+    elseData: '',
+  },
+  {
+    id: 1,
+    taskNumber: 1,
+    title: 'Покрыть тестами первые три домашки',
+    dateBeginning: '10.10.2022',
+    dateEnd: '05.11.2022',
+    status: 0,
+    elseData: '',
+  }
 ]
 
 export const HomeworksPage = () => {
@@ -46,41 +47,41 @@ export const HomeworksPage = () => {
   return (
     <div className='margin-common-content'>
       Домашки
-        <FilterList data={[
-            {
-                id: 1, name: 'Все'
-            },
-            {
-                id: 2, name: 'Эта неделя'
-            },
-            {
-                id: 3, name: 'Этот месяц'
-            }
-        ]} type=''/>
-        <div> </div>
-        <FilterList data={[
-            {
-            id: 1, name: 'Все'
-            },
-            {
-                id: 2, name: 'Эта неделя'
-            },
-            {
-                id: 3, name: 'Этот месяц'
-            },
-            {
-                id: 4, name: 'Этот год'
-            },
-            {
-                id: 5, name: 'Это десятилетие'
-            },
-        ]} type='table'/>
-      
+      <Datepicker/>
+      <FilterList data={[
         {
-            revertedArray.map( item =>
-                <HomeworkCard data={item} key={item.id}/>
-            )
+          id: 1, name: 'Все'
+        },
+        {
+          id: 2, name: 'Эта неделя'
+        },
+        {
+          id: 3, name: 'Этот месяц'
         }
+      ]} type=''/>
+      <FilterList data={[
+        {
+          id: 1, name: 'Все'
+        },
+        {
+          id: 2, name: 'Эта неделя'
+        },
+        {
+          id: 3, name: 'Этот месяц'
+        },
+        {
+          id: 4, name: 'Этот год'
+        },
+        {
+          id: 5, name: 'Это десятилетие'
+        },
+      ]} type='table'/>
+
+      {
+        revertedArray.map(item =>
+          <HomeworkCard data={item} key={item.id}/>
+        )
+      }
     </div>
   )
 }
