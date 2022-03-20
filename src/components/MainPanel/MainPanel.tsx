bad4a5abca3b7c507378874e33bd175585082b0e
Aleksan <sanyok.10rus@gmail.com>
2022-03-20 06:02:43 +0300

    added a collapse button with styles

--- src/components/MainPanel/MainPanel.tsx
+++ src/components/MainPanel/MainPanel.tsx
@@ -1,16 +1,14 @@
 import './MainPanel.scss';
-import devEduLogo from '../../pages/images/devEduLogo.svg';
-import devEduTitle from '../../pages/images/devEduTitle.svg';
 import { Avatar } from './Avatar/Avatar';
 import { Navigation } from './Navigation/Navigation';
 import avatarPhoto from '../images/avatar.png';
 import { Exit } from './Exit/Exit';
 
 import { Toggle } from './Toggle/Toggle';
-import React, { useState, useEffect } from 'react';
-import { SvgLogo } from '../SvgIcon/SvgFiles/logo';
-import { SvgLogoName } from '../SvgIcon/SvgFiles/logoName';
-import { SvgLeftChevron } from '../SvgIcon/SvgFiles/leftChevron';
+import React, { useState } from 'react';
+import { SvgLogo } from '../SvgIcon/SvgFiles/SvgLogo';
+import { SvgLogoName } from '../SvgIcon/SvgFiles/SvgLogoName';
+import { CollapseButton } from './CollapsButton/CollapsButton'
 let avData = {
   photo: avatarPhoto,
   name: 'Антон Ефременков',
@@ -18,6 +16,7 @@ let avData = {
 };
 
 export const MainPanel = () => {
+  
   const [isCollapsed, setIsCollapsed] = useState<boolean>(false);
   const [isToggled, setIsToggled] = useState<boolean>(false);
   const handleClick = () => {
@@ -26,13 +25,7 @@ export const MainPanel = () => {
   
   return (
     <aside className={`main-panel ${ isCollapsed ? 'collapsed': '' }`}>
-      <button className='collapse-button' onClick={()=>setIsCollapsed(!isCollapsed)}>
-        <div className='left-part'>
-          <SvgLeftChevron/>
-        </div>
-        <div className='right-part'/>
-      </button>
-
+      <CollapseButton onClick={()=>setIsCollapsed(!isCollapsed)}/>
       <div className='logo-container'>
         <SvgLogo/>
         <SvgLogoName/>
