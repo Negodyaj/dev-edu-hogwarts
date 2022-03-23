1cbce0ebf75f1db82610ed481122d1a1ceed508a
Polina <rudichrudichrudich@mail.ru>
2022-03-24 01:18:23 +0300

    add component

--- src/pages/SettingsPage/SettingsPage.tsx
+++ src/pages/SettingsPage/SettingsPage.tsx
@@ -1,5 +1,36 @@
+import { useForm, Controller, SubmitHandler } from 'react-hook-form';
+import DatePicker from 'react-datepicker';
+import { NULL } from 'sass';
+export type UserFormData = {
+
+  firstName: string,
+  lastName: string,
+  patronymic: string,
+  email: string,
+  password: string,
+  city: string,
+  birthDate: string,
+  gitHubAccount: string,
+  phoneNumber: string,
+  
+
+}
 export const SettingsPage = () => {
+  const {register, control} = useForm<UserFormData>();
   return (
-    <div>Настройки</div>
+    <>
+      <div>Настройки аккаунта</div>
+      <form>
+        <p>Фамилия</p>
+        <input {...register('lastName')}></input>
+        <p>Имя</p>
+        <input></input>
+        <p>Отчество</p>
+        <input></input>
+        <p>Дата рождения</p>
+      
+        <p>Пароль</p>
+      </form>
+    </>
   )
 }
