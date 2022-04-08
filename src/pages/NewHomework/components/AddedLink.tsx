1571acc988f1419a63f02710196c5708ffc422c1
Aliya <saigafarova00@yandex.ru>
2022-04-09 01:05:41 +0300

    add: new component

---
+++ src/pages/NewHomework/components/AddedLink.tsx
@@ -0,0 +1,25 @@
+import {RemoveLink} from "../../../actions/newHomeworkForm.action";
+import {SvgIcon} from "../../../components/SvgIcon/SvgIcon";
+import {Icon} from "../../../shared/enums/Icon";
+import {useDispatch} from "react-redux";
+
+type AddedLinkProps = {
+  source: string
+  itemNumber: number
+}
+
+export const AddedLink = (props: AddedLinkProps) => {
+  const dispatch = useDispatch();
+
+  return(
+    <div className='form-input_link__container'>
+      <a href={props.source} className='form-link' target="_blank">
+        {props.source}
+      </a>
+      <div onClick={() => dispatch(RemoveLink(props.itemNumber))}
+           className='form-input_link__delete-link'>
+        <SvgIcon icon={Icon.Cross}/>
+      </div>
+    </div>
+  )
+}
