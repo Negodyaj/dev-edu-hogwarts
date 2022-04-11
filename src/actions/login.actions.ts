5ec2ab8ca19e60d46a6720d7c8f9a8336433f77a
Polina <rudichrudichrudich@mail.ru>
2022-04-11 20:55:12 +0300

    added final fixes

---
+++ src/actions/login.actions.ts
@@ -0,0 +1,11 @@
+import { UserResponse } from "../models/responses/UserResponse";
+
+export const SET_CURRENT_USER = 'login/SET_CURRENT_USER' as const;
+export const REHYDRATE = 'persist/REHYDRATE' as const;
+
+export const setCurrentUser = (user: UserResponse) => ({
+  type: SET_CURRENT_USER,
+  payload: user
+});
+export type LoginPageAction = 
+  ReturnType<typeof setCurrentUser>;
