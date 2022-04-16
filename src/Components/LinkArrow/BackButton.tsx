27c99fe2e4e9327579d4effb87c8e56267066660
Aliya <saigafarova00@yandex.ru>
2022-04-16 20:31:28 +0300

    add: back button fix: related files

---
+++ src/Components/LinkArrow/BackButton.tsx
@@ -0,0 +1,13 @@
+import { useNavigate } from 'react-router-dom';
+import { SvgArrow } from '../SvgIcon/SvgFiles/SvgArrow';
+
+export const BackButton = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="link-arrow" onClick={() => navigate(-1)}>
+      <SvgArrow back={true} />
+      Назад
+    </div>
+  );
+};
