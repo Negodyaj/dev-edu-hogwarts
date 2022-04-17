78b0a9945fac24405f9a7b302e18db258cdc8a33
Aliya <saigafarova00@yandex.ru>
2022-04-17 23:31:51 +0300

    fix: minor

--- src/shared/consts.ts
+++ src/shared/consts.ts
@@ -4,4 +4,14 @@ export const loginUrl = 'sign-in';
 export const lessonsUrl = 'lessons';
 export const addNewTaskUrl = 'api/tasks/teacher';
 export const getHomeworkById = (id: number) => `api/homeworks/${id}`;
+export const postStudentAnswer = (homeworkId: number) =>
+  `api/student-homeworks/${homeworkId}`;
+export const studentHomeworkById = (id: number) =>
+  `api/student-homeworks/${id}`;
+export const studentHomeworksByUserId = (userId: number) => {
+  return `api/student-homeworks/by-user/${userId}`;
+};
+// export const editStudentAnswer = (id: number) => {
+//   return `api/student-homeworks/${id}`;
+// };
 export const registerUrl = 'register';
