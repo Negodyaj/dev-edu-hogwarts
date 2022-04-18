434ba211ac971e58ca542cd0b75ebb5de5ecf930
Artur Khamatnurov <arturkh.116@gmail.com>
2022-04-18 15:49:06 +0300

    Add Filter func

--- src/pages/LessonsPage/LessonsPage.tsx
+++ src/pages/LessonsPage/LessonsPage.tsx
@@ -2,31 +2,38 @@ import { useState } from "react";
 import { Lesson, LessonModel } from "./components/Lesson";
 import {TabContainer} from "../../components/TabContainer/TabContainer";
 import {Icon} from "../../shared/enums/Icon";
+import { date } from "yup/lib/locale";
 
 
 export const LessonsPage = () => {
   const [activeLesson, setActiveLesson] = useState(0);
   const lessons: LessonModel[] = [
-    {id: 3, name: 'Занятие 3', date: '10.03.2022', theme: 'Познакомитесь с основами C++ и научитесь создавать простейшие консольные программы.', videoLink: 'https://disk.yandex.com/d/9WeaF1Yua7D1IA', additionalInfo: 'Плагин Figma, позволяет создавать красивые тени, просто перетаскивая «источник света»' },
-    {id: 2, name: 'Занятие 2', date: '14.02.2022', theme: 'Научитесь проектировать быстрые алгоритмы, применять стандартные структуры данных, а главное — мыслить как программист. Знание алгоритмов может повысить ваши шансы на трудоустройство, так как в большинстве компаний задачи на алгоритмы — неотъемлемая часть собеседования и тестового задания.', videoLink: '', additionalInfo: '' },
-    {id: 1, name: 'Занятие 1', date: '10.02.2022', theme: 'В этом модуле вас ждут 10 видеолекций, которые помогут вам начать разговаривать на английском языке как настоящий разработчик.', videoLink: '', additionalInfo: '' },
+    {id: 3, name: 'Занятие 3', date: '3/10/2022', theme: 'Познакомитесь с основами C++ и научитесь создавать простейшие консольные программы.', videoLink: 'https://disk.yandex.com/d/9WeaF1Yua7D1IA', additionalInfo: 'Плагин Figma, позволяет создавать красивые тени, просто перетаскивая «источник света»' },
+    {id: 2, name: 'Занятие 2', date: '2/14/2022', theme: 'Научитесь проектировать быстрые алгоритмы, применять стандартные структуры данных, а главное — мыслить как программист. Знание алгоритмов может повысить ваши шансы на трудоустройство, так как в большинстве компаний задачи на алгоритмы — неотъемлемая часть собеседования и тестового задания.', videoLink: '', additionalInfo: '' },
+    {id: 1, name: 'Занятие 1', date: '2/10/2022', theme: 'В этом модуле вас ждут 10 видеолекций, которые помогут вам начать разговаривать на английском языке как настоящий разработчик.', videoLink: '', additionalInfo: '' },
   ];
 
-  // const toggle = document.querySelector(".circle")
-  // const onElementClick = (id:number) => {
-  //   setActiveLesson(id === activeLesson ? 0 : id)
-  // }
+  
   function onElementClick (id:number) {
-    // if (id === activeLesson) {
-    //   setActiveLesson(0);
-    // }
-    // else
-    setActiveLesson(id === activeLesson ? 0 : id)
     
+    setActiveLesson(id === activeLesson ? 0 : id) 
     
   }
+  const filtered = lessons.filter((elem) => {
+    const date1 = +new Date();
+    const date2 = +new Date(elem.date);
+    const diffTime = Math.abs(date2 - date1);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+    if (diffDays > 7) {
+      return false;
+    } else {
+      return true;
+    }
+  })
+  
+}
 
-  return (
+return (
     <>
       <TabContainer tabContainerData={ [
         {id: 1, icon: Icon.Cookie, text: 'Базовый курс'},
@@ -37,6 +44,11 @@ export const LessonsPage = () => {
       />
       
       <div>Занятия</div>
+      <select name="period-select" id="period-select">
+        <option selected>Эта неделя</option>
+        <option>Этот месяц</option>
+        <option>Всё время</option>
+      </select>
       <div className="lessons-container">
       {
         lessons.map(lesson => <Lesson data={lesson} id={lesson.id} key={lesson.id} activeLessonId={activeLesson} onClick={onElementClick}/>)
@@ -44,4 +56,4 @@ export const LessonsPage = () => {
       </div>
     </>
   )
-}
+  }
