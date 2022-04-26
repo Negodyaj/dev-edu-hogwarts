c898ce1d4ee60aa61a1a0a549884d44bd4a3b9a2
Polina <rudichrudichrudich@mail.ru>
2022-04-26 17:59:21 +0300

    deleted grid

--- src/pages/RegistrationPage/RegistrationPage.tsx
+++ src/pages/RegistrationPage/RegistrationPage.tsx
@@ -46,134 +46,151 @@ export const RegistrationPage = () => {
 
   return (
     <div className="register-form-wrapper content-container">
-      <h2>Регистрация</h2>
+      <h1>Регистрация</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="grid-container-registraton-page">
-          <div className="grid-column-item item-lastName">
-            <label htmlFor="lastName">
-              Фамилия<span className="asterisk"> *</span>
-            </label>
-            <input
-              className="input-style"
-              type="text"
-              placeholder="Ефременков"
-              id="lastName"
-              {...register('lastName', {
-                required: true,
-                maxLength: 20,
-                pattern: /^[А-Я][а-я]+$/i,
-              })}
-            />
-            {errors?.lastName?.type === 'required' && (
-              <p className="asterisk">Обязательно для заполнения</p>
-            )}
-            {errors?.lastName?.type === 'maxLength' && (
-              <p className="asterisk">Превышена допустимая длина 20 символов</p>
-            )}
-            {errors?.lastName?.type === 'pattern' && (
-              <p className="asterisk">Недопустимые символы</p>
-            )}
+        <div>
+          <div className="flex-container">
+            <div>
+              <div className="flex-item item-lastName">
+                <label htmlFor="lastName">
+                  Фамилия<span className="asterisk"> *</span>
+                </label>
+                <input
+                  className="input-style"
+                  type="text"
+                  placeholder="Ефременков"
+                  id="lastName"
+                  {...register('lastName', {
+                    required: true,
+                    maxLength: 20,
+                    pattern: /^[А-Я][а-я]+$/i,
+                  })}
+                />
+                {errors?.lastName?.type === 'required' && (
+                  <p className="asterisk">Обязательно для заполнения</p>
+                )}
+                {errors?.lastName?.type === 'maxLength' && (
+                  <p className="asterisk">
+                    Превышена допустимая длина 20 символов
+                  </p>
+                )}
+                {errors?.lastName?.type === 'pattern' && (
+                  <p className="asterisk">Недопустимые символы</p>
+                )}
+              </div>
+              <div className="flex-item item-firstName">
+                <label htmlFor="firstName">
+                  Имя<span className="asterisk"> *</span>
+                </label>
+                <input
+                  className="input-style"
+                  type="text"
+                  placeholder="Антон"
+                  id="firstName"
+                  {...register('firstName', {
+                    required: true,
+                    maxLength: 20,
+                    pattern: /^[А-Я][а-я]+$/i,
+                  })}
+                />
+                {errors?.firstName?.type === 'required' && (
+                  <p className="asterisk">Обязательно для заполнения</p>
+                )}
+                {errors?.firstName?.type === 'maxLength' && (
+                  <p className="asterisk">
+                    Превышена допустимая длина 20 символов
+                  </p>
+                )}
+                {errors?.firstName?.type === 'pattern' && (
+                  <p className="asterisk">Недопустимые символы</p>
+                )}
+              </div>
+              <div className="flex-item item-datepicker">
+                <label htmlFor="datepicker">Дата рождения</label>
+                <Controller
+                  name="birthdate"
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field }) => <Datepicker field={field} />}
+                />
+              </div>
+            </div>
+            <div className="flex-item item-patronymic">
+              <label htmlFor="patronymic">Отчество</label>
+              <input
+                className="input-style"
+                type="text"
+                placeholder="Сергеевич"
+                {...register('patronymic', {
+                  required: true,
+                  pattern: /^[А-Я][а-я]+$/i,
+                })}
+              />
+            </div>
           </div>
-          <div className="grid-column-item item-firstName">
-            <label htmlFor="firstName">
-              Имя<span className="asterisk"> *</span>
-            </label>
-            <input
-              className="input-style"
-              type="text"
-              placeholder="Антон"
-              id="firstName"
-              {...register('firstName', {
-                required: true,
-                maxLength: 20,
-                pattern: /^[А-Я][а-я]+$/i,
-              })}
-            />
-            {errors?.firstName?.type === 'required' && (
-              <p className="asterisk">Обязательно для заполнения</p>
-            )}
-            {errors?.firstName?.type === 'maxLength' && (
-              <p className="asterisk">Превышена допустимая длина 20 символов</p>
-            )}
-            {errors?.firstName?.type === 'pattern' && (
-              <p className="asterisk">Недопустимые символы</p>
-            )}
-          </div>
-          <div className="grid-column-item item-datepicker">
-            <label htmlFor="datepicker">Дата рождения</label>
-            <Controller
-              name="birthdate"
-              control={control}
-              rules={{ required: true }}
-              render={({ field }) => <Datepicker field={field} />}
-            />
-          </div>
-          <div className="grid-column-item item-password">
-            <label>
-              Пароль<span className="asterisk"> *</span>
-            </label>
-            <input
-              className="custom-password input-style"
-              type="password"
-              {...register('password', {
-                required: true,
-              })}
-            />
-            {errors?.password?.type === 'required' && (
-              <p className="attention">Обязательно для заполнения</p>
-            )}
-          </div>
-          <div className="grid-column-item item-email">
-            <label htmlFor="email">
-              E-mail<span className="asterisk"> *</span>
-            </label>
-            <input
-              className="input-style"
-              type="email"
-              id="email"
-              placeholder="example@example.com"
-              {...register('email', {
-                required: true,
-                pattern:
-                  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
-              })}
-            />
-            {errors?.email?.type === 'required' && (
-              <p className="attention">Обязательно для заполнения</p>
-            )}
-          </div>
-          <div className="grid-column-item item-patronymic">
-            <label htmlFor="patronymic">Отчество</label>
-            <input
-              className="input-style"
-              type="text"
-              placeholder="Сергеевич"
-              {...register('patronymic', {
-                required: true,
-                pattern: /^[А-Я][а-я]+$/i,
-              })}
-            />
-          </div>
-          <div className="grid-column-item item-tel">
-            <label htmlFor="phoneNumber">Телефон</label>
-            <input
-              className="input-style"
-              type="tel"
-              placeholder="+7(999)888-77-66"
-              {...register('phoneNumber', {
-                required: true,
-                pattern: /^[0-9]+$/i,
-              })}
-            />
-            {errors?.phoneNumber?.type === 'pattern' && (
-              <p className="attention">Введите данные корректно</p>
-            )}
-          </div>
-          <div className="grid-column-item item-repeat-password">
-            <label>
-              Повторить пароль<span className="asterisk"> *</span>
-            </label>
-            <input type="password" className="custom-password input-style" />
+          <div className="flex-container">
+            <div>
+              <div className="flex-item item-password">
+                <label>
+                  Пароль<span className="asterisk"> *</span>
+                </label>
+                <input
+                  className="custom-password input-style"
+                  type="password"
+                  {...register('password', {
+                    required: true,
+                  })}
+                />
+                {errors?.password?.type === 'required' && (
+                  <p className="attention">Обязательно для заполнения</p>
+                )}
+              </div>
+              <div className="flex-item item-email">
+                <label htmlFor="email">
+                  E-mail<span className="asterisk"> *</span>
+                </label>
+                <input
+                  className="input-style"
+                  type="email"
+                  id="email"
+                  placeholder="example@example.com"
+                  {...register('email', {
+                    required: true,
+                    pattern:
+                      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
+                  })}
+                />
+                {errors?.email?.type === 'required' && (
+                  <p className="attention">Обязательно для заполнения</p>
+                )}
+              </div>
+            </div>
+            <div>
+              <div className="flex-item item-repeat-password">
+                <label>
+                  Повторить пароль<span className="asterisk"> *</span>
+                </label>
+                <input
+                  type="password"
+                  className="custom-password input-style"
+                />
+              </div>
+              <div className="flex-item item-tel">
+                <label htmlFor="phoneNumber">Телефон</label>
+                <input
+                  className="input-style"
+                  type="tel"
+                  placeholder="+7(999)888-77-66"
+                  {...register('phoneNumber', {
+                    required: true,
+                    pattern: /^[0-9]+$/i,
+                  })}
+                />
+                {errors?.phoneNumber?.type === 'pattern' && (
+                  <p className="attention">Введите данные корректно</p>
+                )}
+              </div>
+            </div>
           </div>
         </div>
         <p className="warning-validation">
