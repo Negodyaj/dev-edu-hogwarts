cb76aa94dd8c14734e23f9d4cab53f7c3298e173
zeta0ne <space1signs@gmail.com>
2022-04-28 00:48:51 +0300

    still wip

--- src/components/NotificationsContainer/NotificationItem/NotificationItem.tsx
+++ src/components/NotificationsContainer/NotificationItem/NotificationItem.tsx
@@ -1,8 +1,32 @@
 import "./NotificationItem.scss"
 
+export type NotificationProps = {
+  data: NotificationData
+}
+
+export type NotificationData = {
+  img: string
+  text: string
+}
+
+/*export const NotificationItem = (props: NotificationProps) => {
+
+  return(
+    <div className="notification-window">
+      <img src={props.data.img} alt="img" />
+      <p>{props.data.text}</p>
+    </div>
+  )
+  
+}*/
+
 export const NotificationItem = () => {
-  <div className="notification-window">
-    <img src="" alt="" />
-    <p></p>
-  </div>
+
+  return(
+    <div className="notification-window">
+      <img src="" alt="img" />
+      <p>Я нотификашка</p>
+    </div>
+  )
+  
 }
--- src/components/NotificationsContainer/NotificationsContainer.tsx
+++ src/components/NotificationsContainer/NotificationsContainer.tsx
@@ -1,6 +1,30 @@
+import { useEffect, useState } from "react";
+import { isTemplateExpression } from "typescript"
+import { NotificationItem } from "./NotificationItem/NotificationItem"
+
+let notifs = [
+  {
+    img: '',
+    text: 'Всё заебись'
+  },
+  {
+    img: '',
+    text: 'Что-то пошло не так'
+  }
+];
+
+
+
 export const NotificationContainer = () => {
+  const [notif, setNotif] = useState('');
+
+  //useEffect(() =>{
+
+  //})
   return(
-    <>
-    </>
+    <div>
+      <button>Нажми меня</button>
+      <NotificationItem />
+    </div>
   )
 }
