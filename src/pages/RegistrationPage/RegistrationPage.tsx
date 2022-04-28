67070442f6de0e467148ab81c7c4960674d3dd18
Aliya <saigafarova00@yandex.ru>
2022-04-29 02:10:12 +0300

    fix: minor

--- src/pages/RegistrationPage/RegistrationPage.tsx
+++ src/pages/RegistrationPage/RegistrationPage.tsx
@@ -1,5 +1,5 @@
 import './RegistrationPage.scss';
-import { Controller, useForm } from 'react-hook-form';
+import { Controller, useForm, FormProvider } from 'react-hook-form';
 import { setToken } from '../../services/auth.service';
 import { baseWretch } from '../../services/base-wretch.service';
 import { registerUrl } from '../../shared/consts';
@@ -10,6 +10,7 @@ import {
 } from '../../components/Button/Button';
 import Datepicker from '../../components/Datepicker/Datepicker';
 import { convertDate } from '../../shared/helpers/dateHelpers';
+import { CheckboxBtn } from '../../components/CheckBoxGroup/CheckBox/CheckBox';
 
 export type RegisterFormData = {
   firstName: string;
@@ -22,12 +23,8 @@ export type RegisterFormData = {
 };
 
 export const RegistrationPage = () => {
-  const {
-    register,
-    handleSubmit,
-    control,
-    formState: { errors },
-  } = useForm<RegisterFormData>();
+  const method = useForm<RegisterFormData>();
+
   const onSubmit = (data: RegisterFormData) =>
     baseWretch()
       .url(registerUrl)
@@ -40,171 +37,182 @@ export const RegistrationPage = () => {
       .text((token) => setToken(token));
 
   return (
-    <div className="register-form-wrapper">
-      <h2>Регистрация</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="form-element">
-          <label htmlFor="lastName">
-            Фамилия
-            <span className="asterisk"> *</span>
-          </label>
-          <input
-            type="text"
-            className="form-input"
-            placeholder="Ефременков"
-            id="lastName"
-            {...register('lastName', {
-              required: true,
-              maxLength: 20,
-              pattern: /^[A-Za-z]+$/i,
-            })}
-          />
-          {errors?.lastName?.type === 'required' && (
-            <p className="asterisk">Обязательно для заполнения</p>
-          )}
-          {errors?.lastName?.type === 'maxLength' && (
-            <p className="asterisk">Превышена допустимая длина 20 символов</p>
-          )}
-          {errors?.lastName?.type === 'pattern' && (
-            <p className="asterisk">Недопустимые символы</p>
-          )}
-        </div>
-        <div className="form-grid-container">
+    <FormProvider {...method}>
+      <div className="register-form-wrapper">
+        <h2>Регистрация</h2>
+        <form onSubmit={method.handleSubmit(onSubmit)}>
           <div className="form-element">
-            <label htmlFor="firstName">
-              Имя<span className="asterisk"> *</span>
+            <label htmlFor="lastName">
+              Фамилия
+              <span className="asterisk"> *</span>
             </label>
             <input
               type="text"
               className="form-input"
-              placeholder="Антон"
-              id="firstName"
-              {...register('firstName', {
+              placeholder="Ефременков"
+              id="lastName"
+              {...method.register('lastName', {
                 required: true,
                 maxLength: 20,
                 pattern: /^[A-Za-z]+$/i,
               })}
             />
-            {errors?.firstName?.type === 'required' && (
+            {method.formState.errors?.lastName?.type === 'required' && (
               <p className="asterisk">Обязательно для заполнения</p>
             )}
-            {errors?.firstName?.type === 'maxLength' && (
+            {method.formState.errors?.lastName?.type === 'maxLength' && (
               <p className="asterisk">Превышена допустимая длина 20 символов</p>
             )}
-            {errors?.firstName?.type === 'pattern' && (
+            {method.formState.errors?.lastName?.type === 'pattern' && (
               <p className="asterisk">Недопустимые символы</p>
             )}
           </div>
-          <div className="form-element">
-            <label htmlFor="patronymic">Отчество</label>
-            <input
-              type="text"
-              className="form-input"
-              placeholder="Сергеевич"
-              {...register('patronymic', {
-                required: true,
-              })}
+          <div className="form-grid-container">
+            <div className="form-element">
+              <label htmlFor="firstName">
+                Имя<span className="asterisk"> *</span>
+              </label>
+              <input
+                type="text"
+                className="form-input"
+                placeholder="Антон"
+                id="firstName"
+                {...method.register('firstName', {
+                  required: true,
+                  maxLength: 20,
+                  pattern: /^[A-Za-z]+$/i,
+                })}
+              />
+              {method.formState.errors?.firstName?.type === 'required' && (
+                <p className="asterisk">Обязательно для заполнения</p>
+              )}
+              {method.formState.errors?.firstName?.type === 'maxLength' && (
+                <p className="asterisk">
+                  Превышена допустимая длина 20 символов
+                </p>
+              )}
+              {method.formState.errors?.firstName?.type === 'pattern' && (
+                <p className="asterisk">Недопустимые символы</p>
+              )}
+            </div>
+            <div className="form-element">
+              <label htmlFor="patronymic">Отчество</label>
+              <input
+                type="text"
+                className="form-input"
+                placeholder="Сергеевич"
+                {...method.register('patronymic', {
+                  required: true,
+                  pattern: /^[А-Я][а-я]+$/i,
+                })}
+              />
+            </div>
+          </div>
+          <div className="form-grid-container">
+            <div className="form-element">
+              <label htmlFor="datepicker">Дата рождения</label>
+              <Controller
+                name="birthDate"
+                control={method.control}
+                rules={{ required: true }}
+                render={({ field }) => <Datepicker field={field} />}
+              />
+            </div>
+          </div>
+          <div className="form-grid-container">
+            <div className="form-element">
+              <label htmlFor="password">
+                Пароль<span className="asterisk"> *</span>
+              </label>
+              <input
+                type="password"
+                className="form-input"
+                {...method.register('password', {
+                  required: true,
+                })}
+              />
+              {method.formState.errors?.password?.type === 'required' && (
+                <p className="attention">Обязательно для заполнения</p>
+              )}
+            </div>
+            <div className="form-element">
+              <label htmlFor="repeat-password">
+                Повторить пароль<span className="asterisk"> *</span>
+              </label>
+              <input type="password" className="form-input" />
+            </div>
+          </div>
+          <div className="form-grid-container">
+            <div className="form-element">
+              <label htmlFor="email">
+                E-mail<span className="asterisk"> *</span>
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="form-input"
+                placeholder="example@example.com"
+                {...method.register('email', {
+                  required: true,
+                  pattern:
+                    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
+                })}
+              />
+              {method.formState.errors?.email?.type === 'required' && (
+                <p className="attention">Обязательно для заполнения</p>
+              )}
+            </div>
+            <div className="form-element">
+              <label htmlFor="phoneNumber">Телефон</label>
+              <input
+                type="tel"
+                className="form-input"
+                placeholder="+7(999)888-77-66"
+                {...method.register('phoneNumber', {
+                  required: true,
+                  pattern: /^[0-9]+$/i,
+                })}
+              />
+              {method.formState.errors?.phoneNumber?.type === 'pattern' && (
+                <p className="attention">Введите данные корректно</p>
+              )}
+            </div>
+          </div>
+          <p className="warning-validation">
+            <span className="attention">* </span>
+            Поля обязательные для заполнения
+          </p>
+          <div className="buttons">
+            <Button
+              text="Зарегистрироваться"
+              model={ButtonModel.Colored}
+              type={ButtonType.submit}
+              width="238"
+            />
+            <Button
+              text="Отмена"
+              model={ButtonModel.Text}
+              type={ButtonType.reset}
+              width="190"
             />
           </div>
-        </div>
-        <div className="form-grid-container">
-          <div className="form-element">
-            <label htmlFor="datepicker">Дата рождения</label>
-            <Controller
-              name="birthDate"
-              control={control}
-              rules={{ required: true }}
-              render={({ field }) => <Datepicker field={field} />}
+          <div className="flex-container">
+            <CheckboxBtn
+              data={{
+                value: 0,
+                text: '',
+                isChecked: false,
+              }}
+              name="policy"
+              isSingle={true}
             />
-          </div>
-        </div>
-        <div className="form-grid-container">
-          <div className="form-element">
-            <label htmlFor="password">
-              Пароль<span className="asterisk"> *</span>
+            <label htmlFor="policy" id="policy-label">
+              Настоящим подтверждаю, что я ознакомлен <br />и согласен с
+              условиями политики конфиденциальности
             </label>
-            <input
-              type="password"
-              className="form-input"
-              {...register('password', {
-                required: true,
-              })}
-            />
-            {errors?.password?.type === 'required' && (
-              <p className="attention">Обязательно для заполнения</p>
-            )}
           </div>
-          <div className="form-element">
-            <label htmlFor="repeat-password">
-              Повторить пароль<span className="asterisk"> *</span>
-            </label>
-            <input type="password" className="form-input" />
-          </div>
-        </div>
-        <div className="form-grid-container">
-          <div className="form-element">
-            <label htmlFor="email">
-              E-mail<span className="asterisk"> *</span>
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="form-input"
-              placeholder="example@example.com"
-              {...register('email', {
-                required: true,
-                pattern:
-                  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
-              })}
-            />
-            {errors?.email?.type === 'required' && (
-              <p className="attention">Обязательно для заполнения</p>
-            )}
-          </div>
-          <div className="form-element">
-            <label htmlFor="phoneNumber">Телефон</label>
-            <input
-              type="tel"
-              className="form-input"
-              placeholder="+7(999)888-77-66"
-              {...register('phoneNumber', {
-                required: true,
-              })}
-            />
-          </div>
-        </div>
-        <p className="warning-validation">
-          <span className="attention">* </span>
-          Поля обязательные для заполнения
-        </p>
-        <div className="buttons">
-          <Button
-            text="Зарегистрироваться"
-            model={ButtonModel.Colored}
-            type={ButtonType.submit}
-            width="238"
-          />
-          <Button
-            text="Отмена"
-            model={ButtonModel.Text}
-            type={ButtonType.reset}
-            width="190"
-          />
-        </div>
-        <div className="flex-container">
-          <CheckboxBtn
-            data={{
-              name: 'policy',
-              value: 0,
-              text: '',
-            }}
-          />
-          <label htmlFor="policy" id="policy-label">
-            Настоящим подтверждаю, что я ознакомлен <br />и согласен с условиями
-            политики конфиденциальности
-          </label>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
+    </FormProvider>
   );
 };
