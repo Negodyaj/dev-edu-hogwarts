d1ed552823c2fbced3bab6b33791cb47b3ea4c59
zeta0ne <space1signs@gmail.com>
2022-05-12 19:46:26 +0300

    linter fixes

--- src/components/NotificationsContainer/NotificationItem/NotificationItem.tsx
+++ src/components/NotificationsContainer/NotificationItem/NotificationItem.tsx
@@ -1,32 +1,38 @@
-import { useEffect, useState } from 'react'
-import "./NotificationItem.scss"
+import { useEffect, useState } from 'react';
+import './NotificationItem.scss';
 
 export type NotificationProps = {
-  data: NotificationData
-}
+  data: NotificationData;
+};
 
 export type NotificationData = {
-  img: any
-  text: string
-  type: string
-}
+  img: any;
+  text: string;
+  type: string;
+};
 
 export const NotificationItem = (props: NotificationProps) => {
-const [invisible, setInvisible] = useState('')
+  const [invisible, setInvisible] = useState('');
   useEffect(() => {
-    if (props.data.type === 'good'){
+    if (props.data.type === 'good') {
       setTimeout(() => setInvisible('invisible'), 2500);
     } else {
       setTimeout(() => setInvisible('invisible'), 4000);
     }
-  }, [])
+  }, []);
 
-  return(
+  return (
     <>
-      <div onClick={()=>{setInvisible('invisible')}} className={`notification-window ${invisible}`}>
+      <div
+        onClick={() => {
+          setInvisible('invisible');
+        }}
+        className={`notification-window ${invisible}
+        `}
+      >
         {props.data.img}
         <p className="notification-text">{props.data.text}</p>
       </div>
     </>
-  )
-}
+  );
+};
--- src/components/NotificationsContainer/NotificationsContainer.tsx
+++ src/components/NotificationsContainer/NotificationsContainer.tsx
@@ -1,15 +1,15 @@
-import { useEffect, useState } from "react";
-import { NotificationItem } from "./NotificationItem/NotificationItem"
-import "./NotificationsContainer.scss";
-import {SvgOk} from '../SvgIcon/SvgFiles/NotificationSvg/SvgOk';
-import {SvgFail} from '../SvgIcon/SvgFiles/NotificationSvg/SvgFail';
+import { useState } from 'react';
+import { NotificationItem } from './NotificationItem/NotificationItem';
+import './NotificationsContainer.scss';
+import { SvgOk } from '../SvgIcon/SvgFiles/NotificationSvg/SvgOk';
+import { SvgFail } from '../SvgIcon/SvgFiles/NotificationSvg/SvgFail';
 import React from 'react';
 
 export type Notifs = {
   img: any;
   text: string;
   type: string;
-}
+};
 
 const notifs: Notifs[] = [
   {
@@ -21,29 +21,27 @@ const notifs: Notifs[] = [
     img: <SvgFail />,
     text: 'Что-то пошло не так =(',
     type: 'bad',
-  }];
+  },
+];
 
 export function NotificationsContainer() {
-  const empty: any[] = []
+  const empty: any[] = [];
   const [containerState, setContainerState] = useState(empty);
 
   function handleClick(notif: Notifs) {
-    setContainerState(containerState.concat(notif))
+    setContainerState(containerState.concat(notif));
   }
 
-  return(
+  return (
     <>
       <button onClick={() => handleClick(notifs[0])}>Хороший нотиф</button>
       <button onClick={() => handleClick(notifs[1])}>Плохой нотиф</button>
-      
+
       <div className="notifications-container">
         {containerState.map((item, i) => (
-          <NotificationItem
-            data={item}
-            key={i}
-          />
+          <NotificationItem data={item} key={i} />
         ))}
       </div>
     </>
-  )
+  );
 }
