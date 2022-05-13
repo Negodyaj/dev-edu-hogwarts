ef7a2d4ff049c1ca239d7e773c4b5765bb6b26da
Aliya <saigafarova00@yandex.ru>
2022-05-13 18:08:16 +0300

    fix: active auth link;

--- src/components/MainPanel/Navigation/Navigation.tsx
+++ src/components/MainPanel/Navigation/Navigation.tsx
@@ -3,7 +3,7 @@ import { ButtonNavigation } from '../ButtonNavigation/ButtonNavigation';
 import { useSelector } from 'react-redux';
 import { LoginPageState } from '../../../store/reducers/login.reducer';
 import { AppState } from '../../../store/store';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { getNavLinksByRole } from './navLinksProvider';
 import { MainPanelState } from '../../../store/reducers/mainPanel.reducer';
 
@@ -12,6 +12,7 @@ export const Navigation = () => {
     (state: AppState) => state.loginPageState as LoginPageState
   );
   const { isCollapsed } = useSelector((state: AppState) => state.mainPanelState as MainPanelState);
+  const location = useLocation();
 
   return (
     <nav className="main-nav-panel">
@@ -20,16 +21,20 @@ export const Navigation = () => {
           <ButtonNavigation isCollapsed={isCollapsed} data={item} key={item?.path} />
         ))
       ) : (
-        <>
-          <Link className="login-link" to={'/login'}>
+        <div className="flex-container">
+          <Link
+            className={`auth-link${location.pathname === '/login' ? ' active-auth-link' : ''}`}
+            to={'/login'}
+          >
             Вход
           </Link>
-          <div>
-            <Link className="register-link" to={'/register'}>
-              Регистрация
-            </Link>
-          </div>
-        </>
+          <Link
+            className={`auth-link${location.pathname === '/register' ? ' active-auth-link' : ''}`}
+            to={'/register'}
+          >
+            Регистрация
+          </Link>
+        </div>
       )}
     </nav>
   );
