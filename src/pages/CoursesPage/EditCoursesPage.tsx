162dd460820886875c811cd3a56e71f40473d4ce
zeta0ne <space1signs@gmail.com>
2022-05-15 04:02:41 +0300

    first version done

--- src/pages/CoursesPage/EditCoursesPage.tsx
+++ src/pages/CoursesPage/EditCoursesPage.tsx
@@ -2,9 +2,26 @@ import { ListView } from './ListView/ListView';
 import { DragDropContext, DragUpdate } from 'react-beautiful-dnd';
 import { useState } from 'react';
 import { lessons } from './ListView/exampleData';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { Button, ButtonModel, ButtonType } from '../../components/Button/Button';
+import './EditCoursesPage.scss';
+
+//вопросы: 
+//как сгенерить айдишник (он должен с бэка приходить вроде?)
+//как сделать так чтобы при отправке темы в массив поля ресетились
+//после нажатия ресет можно отправить пустой инпут (сделать onError?)
+//цифра меняется автоматически, но постится правильная только если один раз нажать на инпут с ней
+
+export type UserFormData = {
+    id: number;
+    lessonNumber: number;
+    lessonName: string;
+    hoursCount: number;
+};
 
 export const EditCoursesPage = () => {
   const [lessonsData, setLessonsData] = useState(lessons); // Это типа данные, которые нам придут
+  const { register, handleSubmit, formState: { errors } } = useForm<UserFormData>();
 
   const onDragEnd = (result: DragUpdate) => {
     const { destination, source, draggableId } = result;
@@ -27,9 +44,52 @@ export const EditCoursesPage = () => {
     setLessonsData(() => [...newLessonsArray]);
   };
 
+  const onSubmit = (data: UserFormData) => {
+    setLessonsData(lessonsData.concat(data))
+    //console.log(lessonsData, data);
+  };
+
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <ListView data={lessonsData} groupId={1} edit={true} />
-    </DragDropContext>
+    <>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <ListView data={lessonsData} groupId={1} edit={true} />
+      </DragDropContext>
+
+      <div className="form-container">
+        <h2>Новая тема</h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="inputs">
+            <div>
+              <span>Тема</span>
+              <input className="form-input short" defaultValue={lessonsData.length+1} {...register("lessonNumber", { required: true })}/>
+            </div>
+            <div>
+              <span>Название</span>
+              <input className="form-input long" placeholder="Введите текст" {...register("lessonName", { required: true })} />
+              {errors.lessonName && <span>Введи название темы</span>}
+            </div>
+            <div>
+              <span>Часы</span>
+              <input className="form-input short" type="number" placeholder="XX" {...register("hoursCount", { required: true })} />
+              {errors.hoursCount && <span>Введи часы</span>}
+            </div>
+          </div>
+          <div className="button-container">
+            <Button
+              text="Сохранить"
+              model={ButtonModel.Colored}
+              type={ButtonType.submit}
+            />
+            <Button 
+              text="Отмена"
+              type={ButtonType.reset}
+              model={ButtonModel.Text} 
+            />
+          </div>
+          
+        </form>
+      </div>
+      
+    </>
   );
 };
