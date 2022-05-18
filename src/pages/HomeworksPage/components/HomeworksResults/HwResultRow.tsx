f26d5b9fd6f20165930cdfab1430752852adca1a
Aleksander <sanyok.10rus@gmail.com>
2022-05-18 05:11:33 +0300

    fixed answers row

--- src/pages/HomeworksPage/components/HomeworksResults/HwResultRow.tsx
+++ src/pages/HomeworksPage/components/HomeworksResults/HwResultRow.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { LinkArrow } from '../../../../components/LinkArrow/LinkArrow';
 import { HomeworkStatus } from '../../../../models/HomeworkCardData';
 import { AnswerResponse } from './Responses/AnswerResponse';
@@ -20,20 +19,15 @@ const getResult = (status: HomeworkStatus) => {
 };
 
 export const HwResultRow = (props: HwResultRowProps) => {
-  const [goal, setGoal] = useState('');
-  const [result, setResult] = useState('');
-
-  useEffect(() => {
-    setGoal(getGoal(props.data.status));
-    setResult(getResult(props.data.status));
-  }, []);
+  const goal = getGoal(props.data.status);
+  const result = getResult(props.data.status);
 
   return (
     <div className="table-row">
       <div>{`${props.data.user.lastName} ${props.data.user.firstName}`}</div>
       <div>{goal}</div>
       <div>{result}</div>
-      <LinkArrow text="смотреть" to={`check-homework/${''}`} />
+      <LinkArrow text="смотреть" to={`check-homework/${props.data.id}`} />
     </div>
   );
 };
