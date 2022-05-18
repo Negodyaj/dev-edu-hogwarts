e34b72804f6d1f839ba8c363bfc0aded5a697b2c
Artur Khamatnurov <arturkh.116@gmail.com>
2022-05-19 00:08:05 +0300

    fix: naming

--- src/actions/lessons.thunks.ts
+++ src/actions/lessons.thunks.ts
@@ -1,7 +1,7 @@
 import { Dispatch } from 'react';
 import { LessonResponse } from '../models/responses/LessonResponse';
 import { baseWretch } from '../services/base-wretch.service';
-import { getLessonsByGroupId } from '../shared/consts';
+import { LessonsByGroupId } from '../shared/consts';
 import { LessonsPageActions, loadLessonsStarted, loadLessonsSuccess } from './lessons.actions';
 
 export const loadLessons = (groupId: number) => {
@@ -9,7 +9,7 @@ export const loadLessons = (groupId: number) => {
     dispatch(loadLessonsStarted());
 
     baseWretch()
-      .url(getLessonsByGroupId(groupId))
+      .url(LessonsByGroupId(groupId))
       .get()
       .json((data) => dispatch(loadLessonsSuccess(data as LessonResponse[])));
   };
