da25b6aeeca5999363fe1e3164f3aca77579ac03
Aleksander <sanyok.10rus@gmail.com>
2022-06-02 18:49:48 +0300

    added styles for main panel

---
+++ src/components/MainPanel/styled/StyledMainPanel.tsx
@@ -0,0 +1,36 @@
+import { useSelector } from 'react-redux';
+import styled, { css } from 'styled-components';
+import { darkTheme, defaultTheme } from '../../../shared/colors';
+import { MainPanelState } from '../../../store/reducers/mainPanel.reducer';
+import { AppState } from '../../../store/store';
+
+export const StyledMainPanel = styled.aside`
+  ${() => {
+    const { isDark } = useSelector((state: AppState) => state.mainPanelState as MainPanelState);
+    return css`
+      background-color: ${isDark ? darkTheme.whiteColor : defaultTheme.whiteColor};
+      path {
+        stroke: ${isDark ? darkTheme.blackColor : defaultTheme.blackColor};
+      }
+      .with-color-on-hover {
+        fill: ${isDark ? darkTheme.blackColor : defaultTheme.blackColor};
+      }
+      a {
+        color: ${isDark ? darkTheme.blackColor : defaultTheme.blackColor};
+      }
+      .auth-link {
+        color: ${isDark ? darkTheme.blackColor : defaultTheme.blackColor};
+      }
+      .nav-link.active,
+      .nav-link:hover {
+        color: ${isDark ? darkTheme.whiteColor : defaultTheme.whiteColor};
+        path {
+          stroke: ${isDark ? darkTheme.whiteColor : defaultTheme.whiteColor};
+        }
+        .with-color-on-hover {
+          fill: ${isDark ? darkTheme.whiteColor : defaultTheme.whiteColor};
+        }
+      }
+    `;
+  }}
+`;
