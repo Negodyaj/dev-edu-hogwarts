863e25516c87cbe7204ae4deb8a07ef4bc581b70
Aleksander <sanyok.10rus@gmail.com>
2022-06-02 18:07:50 +0300

    icons fixed

--- src/components/TabContainer/styled/StyledTabItem.tsx
+++ src/components/TabContainer/styled/StyledTabItem.tsx
@@ -10,8 +10,20 @@ export const StyledTabItem = styled.div<StyledTabItemProps>`
     return css`
       color: ${isDark ? darkTheme.blackColor : defaultTheme.blackColor};
       background-color: ${isDark ? darkTheme.whiteColor : defaultTheme.whiteColor};
+      path {
+        stroke: ${isDark ? darkTheme.blackColor : defaultTheme.blackColor};
+      }
+      .with-color-on-hover {
+        fill: ${isDark ? darkTheme.blackColor : defaultTheme.blackColor};
+      }
       &.active-tab {
         color: ${isDark ? darkTheme.whiteColor : defaultTheme.whiteColor};
+        path {
+          stroke: ${isDark ? darkTheme.whiteColor : defaultTheme.whiteColor};
+        }
+        .with-color-on-hover {
+          fill: ${isDark ? darkTheme.whiteColor : defaultTheme.whiteColor};
+        }
       }
     `;
   }}
