1c946112e8caf568cd1f5bdab9695d6e9d6ca5c3
Polina <rudichrudichrudich@mail.ru>
2022-06-05 19:19:21 +0300

    add files from another branch

---
+++ src/components/styled/Input.tsx
@@ -0,0 +1,36 @@
+import { RegisterOptions, UseFormRegister } from 'react-hook-form';
+import { useSelector } from 'react-redux';
+import { MainPanelState } from '../../store/reducers/mainPanel.reducer';
+import { AppState } from '../../store/store';
+import { StyledInput } from './StyledInput';
+
+export type InputProps = {
+  defaultValue?: any;
+  name: string;
+  width?: string;
+  textAlign?: string;
+  placeholder?: string;
+  type: string;
+  customClassName?: string;
+  isInvalid?: boolean;
+  required?: boolean;
+  register: UseFormRegister<any>;
+  rules?: RegisterOptions;
+};
+export const Input = (props: InputProps) => {
+  const { isDark } = useSelector((state: AppState) => state.mainPanelState as MainPanelState);
+  return (
+    <>
+      <StyledInput
+        placeholder={props.placeholder}
+        type={props.type}
+        inputProps={props}
+        width={props.width}
+        isDark={isDark}
+        defaultValue={props.defaultValue}
+        className={`form-input ${props.customClassName ? props.customClassName : ''}`}
+        {...props.register(`${props.name}`, props.rules)}
+      />
+    </>
+  );
+};
