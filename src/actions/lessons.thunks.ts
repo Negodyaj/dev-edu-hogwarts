a466c7fa40062e31971365bf6c814b22013e5adf
Elena Breslavskaya <elenabreslavskaya103@gmail.com>
2022-06-08 23:04:51 +0300

    delete loader from lessonsPage

--- src/actions/lessons.thunks.ts
+++ src/actions/lessons.thunks.ts
@@ -3,18 +3,18 @@ import { LessonResponse } from '../models/responses/LessonResponse';
 import { baseWretch } from '../services/base-wretch.service';
 import { lessonsByGroupId } from '../shared/consts';
 import { LessonsPageActions, loadLessonsSuccess } from './lessons.actions';
-import { DecrementLoader, IncrementLoader, LoaderAction } from './loader.action';
+import { LoaderAction } from './loader.action';
 
 export const loadLessons = (groupId: number) => {
   return (dispatch: Dispatch<LessonsPageActions | LoaderAction>) => {
-    dispatch(IncrementLoader());
+    //dispatch(IncrementLoader());
 
     baseWretch()
       .url(lessonsByGroupId(groupId))
       .get()
       .json((data) => {
         dispatch(loadLessonsSuccess(data as LessonResponse[]));
-        dispatch(DecrementLoader());
+        //dispatch(DecrementLoader());
       });
   };
 };
