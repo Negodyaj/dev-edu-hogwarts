ec3191010cd1ac8c23f57a24441d0cb11f616f49
Aleksander <sanyok.10rus@gmail.com>
2022-06-09 04:43:07 +0300

    added method to leave editing

--- src/pages/NewLessonPage/NewLessonPage.tsx
+++ src/pages/NewLessonPage/NewLessonPage.tsx
@@ -10,6 +10,7 @@ import Datepicker from '../../components/Datepicker/Datepicker';
 import { LinkWithUnderline } from '../../components/LinkWithUnderline/LinkWithUnderline';
 import { RadioData } from '../../components/RadioGroup/RadioButton/RadioButton';
 import { RadioGroup } from '../../components/RadioGroup/RadioGroup';
+import { UserRole } from '../../shared/enums/UserRole';
 import { LessonsPageState } from '../../store/reducers/lessons.reducer';
 import { LoginPageState } from '../../store/reducers/login.reducer';
 import { NewLessonPageState } from '../../store/reducers/newLessonPage.reducer';
@@ -38,6 +39,7 @@ export const NewLessonPage = () => {
   );
   const { currentUser } = useSelector((state: AppState) => state.loginPageState as LoginPageState);
   const navigate = useNavigate();
+  const { currentRole } = useSelector((state: AppState) => state.loginPageState as LoginPageState);
 
   const initFields = () => {
     if (id) {
@@ -66,6 +68,14 @@ export const NewLessonPage = () => {
   } = methods;
 
   useEffect(() => {
+    if (
+      !(
+        currentRole === UserRole.Teacher ||
+        currentRole === UserRole.Admin ||
+        currentRole === UserRole.Tutor
+      )
+    )
+      navigate('/lessons', { replace: true });
     reset(lessonsData);
   }, [lessonsData]);
 
@@ -145,7 +155,7 @@ export const NewLessonPage = () => {
             className={`form-input${errors.name ? ' invalid-input' : ''}`}
             type="text"
             placeholder="Введите название"
-            {...register('name', { required: true })}
+            {...register('name', { required: 'Поле обязательно к заполнению' })}
           />
         </div>
         <div className="invalid-feedback">{errors.name?.message}</div>
@@ -155,7 +165,14 @@ export const NewLessonPage = () => {
             className={`form-input${errors.linkToRecord ? ' invalid-input' : ''}`}
             type="text"
             placeholder="Ссылка на видео"
-            {...register('linkToRecord', { required: true })}
+            {...register('linkToRecord', {
+              required: 'Поле обязательно к заполнению',
+              pattern: {
+                value:
+                  /^((ftp|http|https):\/\/)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9\-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-\/])*)?/,
+                message: 'Некорректная ссылка',
+              },
+            })}
           />
         </div>
         <div className="invalid-feedback">{errors.linkToRecord?.message}</div>
@@ -164,7 +181,7 @@ export const NewLessonPage = () => {
           <textarea
             className={`form-input${errors.additionalMaterials ? ' invalid-input' : ''}`}
             placeholder="Введите текст"
-            {...register('additionalMaterials', { required: true })}
+            {...register('additionalMaterials', { required: 'Поле обязательно к заполнению' })}
           />
           <div className="invalid-feedback">{errors.additionalMaterials?.message}</div>
         </div>
